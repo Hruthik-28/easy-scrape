3d784d7d1b397467d9f4ca41332c9bf656bac020
hruthik ks <hruthik9980819630@gmail.com>
2025-01-01 12:39:06 +0530

    fix bug: new node creation resulting in LAUNCH_BROWSER type

--- app/workflow/_components/nodes/NodeComponent.tsx
+++ app/workflow/_components/nodes/NodeComponent.tsx
@@ -5,10 +5,11 @@ import NodeHeader from "./NodeHeader";
 import { AppNodeData } from "@/types/appNode";
 import { TaskRegistry } from "@/lib/workflow/task/TaskRegistry";
 import { NodeInputs, NodeInput } from "./NodeInputs";
+import { NodeOutput, NodeOutputs } from "./NodeOutputs";
 
 const NodeComponent = memo((props: NodeProps) => {
   const nodeData = props.data as AppNodeData;
-  const nodeTask = TaskRegistry.LAUNCH_BROWSER;
+  const nodeTask = TaskRegistry[nodeData.type];
   return (
     <NodeCard nodeId={props.id} isSelected={!!props.selected}>
       <NodeHeader taskType={nodeData.type} />
@@ -17,6 +18,11 @@ const NodeComponent = memo((props: NodeProps) => {
           <NodeInput key={taskInput.name} input={taskInput} nodeId={props.id} />
         ))}
       </NodeInputs>
+      <NodeOutputs>
+        {nodeTask.outputs.map((taskOuput) => (
+          <NodeOutput key={taskOuput.name} output={taskOuput} />
+        ))}
+      </NodeOutputs>
     </NodeCard>
   );
 });
