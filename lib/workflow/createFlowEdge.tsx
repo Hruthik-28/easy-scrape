83f5b5744e2fff8d477c1ae10f897008da0533e1
hruthik ks <hruthik9980819630@gmail.com>
2025-01-04 17:22:32 +0530

    feat: 1. modfied createFlowEdge function 2. Defined ExtractTextFromElementTask 3. add credits field to all tasks 4. Add more type safety for TaskRegistry by defining Registry

--- lib/workflow/createFlowEdge.tsx
+++ lib/workflow/createFlowEdge.tsx
@@ -2,15 +2,10 @@ import { AppEdge } from "@/types/appEdge";
 import { Connection } from "@xyflow/react";
 
 export function CreateFlowEdge(connection: Connection): AppEdge {
-  const { source, target, sourceHandle, targetHandle } = connection;
-
   return {
-    id: source + " -> " + target,
-    source,
-    target,
+    id: crypto.randomUUID(),
+    ...connection,
     type: "EasyScrapeEdge",
     animated: true,
-    sourceHandle,
-    targetHandle,
   };
 }
